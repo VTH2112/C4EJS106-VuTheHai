6249b4d0d3858b21954f9083ff436a16410998ae
VTH2112 <vuthehai211212@gmail.com>
2021-01-23 19:48:19 +0700

    first commit

---
+++ lesson1/leson1.js
@@ -0,0 +1,9 @@
+// let name = prompt("Nhập vào tên của bạn !");
+// let age = prompt("Nhập tuổi của bạn !");
+// let date = prompt("Nhập ngày sinh của bạn!");
+
+
+// console.log(`Họ và tên :  ${name}.toUpperCase(); \nTuổi : ${age}\nNgày Sing : ${date}`);
+console.log( typeof(name));
+// console.log("Tuổi : " + age);
+// console.log("Ngày sinh : " + date);
